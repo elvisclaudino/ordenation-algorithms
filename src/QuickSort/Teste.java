952a6fac6e165627f6a41ce216ec6b01cad17296
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-11-12 20:17:05 -0300

    [update]: pivot

--- src/QuickSort/Teste.java
+++ src/QuickSort/Teste.java
@@ -5,6 +5,8 @@ public class Teste {
         QuickSort quickSort = new QuickSort(); // Instancia a classe QuickSort
         int[] array = {49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28}; // Cria um array de inteiros
 
+        // O Pivot será o ultimo elemento
+
         System.out.println("Array desordenado: "); // Exibe o array desordenado
         quickSort.printArray(array); // Chama o método printArray para exibir o array
         System.out.println(); // Pula uma linha
