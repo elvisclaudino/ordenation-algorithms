3afd2d94308ea4b2aad43deb61b88ebbb657ea4a
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-11-12 18:47:53 -0300

    bubble

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -1,4 +1,30 @@
 package BubbleSort;
 
 public class BubbleSort {
+
+    public void BubbleSort(int[] array){
+
+        // Bubble Sort - O(n^2)
+        int aux; // armazena os valores temporariamente
+        for (int i=0; i < array.length; ++i){ //compara para quando ser necessario fazer a troca
+            for (int j=i; j < array.length; ++j){ //
+                if (array[i]>array[j]){
+                    aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                }
+            }
+        }
+
+        for (int i=0; i<array.length; ++i)
+            System.out.println(array[i] + " ");
+    }
+
+    public void printArray(int[] array) {
+        for(int value : array) { // Percorre o array
+            System.out.print(value + " "); // Exibe o valor atual
+        }
+        System.out.println(); // Pula uma linha
+    }
 }
+
--- src/BubbleSort/Teste.java
+++ src/BubbleSort/Teste.java
@@ -1,4 +1,20 @@
 package BubbleSort;
 
+
+
 public class Teste {
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort(); // Instancia a classe bubbleSort
+        int[] array = {49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28}; // Cria um array de inteiros
+
+        System.out.println("Array desordenado: "); // Exibe o array desordenado
+        bubbleSort.printArray(array); // Chama o método printArray para exibir o array
+        System.out.println(); // Pula uma linha
+
+        bubbleSort.BubbleSort(array); // Chama o método BubbleSort para ordenar o array
+        System.out.println(); // Pula uma linha
+
+        System.out.println("Array ordenado: "); // Exibe o array ordenado
+        bubbleSort.printArray(array); // Chama o método printArray para exibir o array
+    }
 }
